c2c084c7a10044f8b6477c9a3716f2a8105d93c4
SamuelGavilanN <SAMUEL.GAVILAN@correoaiep.cl>
2026-07-29 10:16:14 -0400

    Update AD02Captura.tsx

--- src/components/Transactions/AD/AD02Captura.tsx
+++ src/components/Transactions/AD/AD02Captura.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { supabase } from '../../../lib/supabase';
 import { auth } from '../../../lib/auth';
-import { Html5Qrcode } from 'html5-qrcode';
+import { Html5Qrcode, Html5QrcodeSupportedFormats } from 'html5-qrcode';
 import './AD02.css';
 
 interface Auditoria {
@@ -235,14 +235,14 @@ const AD02Captura: React.FC = () => {
         const html5QrCode = new Html5Qrcode("reader", {
           verbose: false,
           formatsToSupport: [
-            Html5Qrcode.SupportedFormats.CODE_128,
-            Html5Qrcode.SupportedFormats.EAN_13,
-            Html5Qrcode.SupportedFormats.EAN_8,
-            Html5Qrcode.SupportedFormats.CODE_39,
-            Html5Qrcode.SupportedFormats.UPC_A,
-            Html5Qrcode.SupportedFormats.UPC_E,
+            Html5QrcodeSupportedFormats.CODE_128,
+            Html5QrcodeSupportedFormats.EAN_13,
+            Html5QrcodeSupportedFormats.EAN_8,
+            Html5QrcodeSupportedFormats.CODE_39,
+            Html5QrcodeSupportedFormats.UPC_A,
+            Html5QrcodeSupportedFormats.UPC_E,
           ]
-        });
+          });
         html5QrCodeRef.current = html5QrCode;
         
         const cameras = await Html5Qrcode.getCameras();
