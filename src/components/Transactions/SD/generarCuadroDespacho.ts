1ba0c3bbd0ad3f477216879b42a9ea230b9872ae
SamuelGavilanN <SAMUEL.GAVILAN@correoaiep.cl>
2026-08-27 16:56:59 -0400

    Update generarCuadroDespacho.ts

--- src/components/Transactions/SD/generarCuadroDespacho.ts
+++ src/components/Transactions/SD/generarCuadroDespacho.ts
@@ -81,6 +81,7 @@ export function generarCuadroHTML(datos: TransporteData): string {
   const selloLateral = escaparHTML(datos.selloLateral);
   const selloAdicional = escaparHTML(datos.selloAdicional);
   const actas = escaparHTML(datos.actasInformadas);
+  const administrativo = escaparHTML(datos.administrativo);
   const totalPallets = datos.totalPallets || 0;
 
   let htmlLocales = '';
@@ -116,6 +117,10 @@ export function generarCuadroHTML(datos: TransporteData): string {
         <tr>
           <td colspan="2" bgcolor="#F8CBAD" style="border:1px solid #000; padding:5px; font-weight:bold;">Actas Entrega</td>
           <td colspan="5" style="border:1px solid #000; padding:5px;">${actas}</td>
+        </tr>
+        <tr>
+          <td colspan="2" bgcolor="#F8CBAD" style="border:1px solid #000; padding:5px; font-weight:bold;">Administrativo</td>
+          <td colspan="5" style="border:1px solid #000; padding:5px;">${administrativo}</td>
         </tr>
         <tr>
           <td colspan="1" bgcolor="#F8CBAD" style="border:1px solid #000; padding:5px; font-weight:bold;">Fecha Entrega</td>
