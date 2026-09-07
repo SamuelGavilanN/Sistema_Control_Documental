9e9bf501e14d80671b76e7560310f723e79a89c0
SamuelGavilanN <SAMUEL.GAVILAN@correoaiep.cl>
2026-09-07 11:16:12 -0300

    Update Sidebar.tsx

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -39,6 +39,7 @@ const menuSections: MenuSection[] = [
     { id: 'sd-analisis-bultos', label: 'SD04 Análisis Bultos Desp', type: 'subitem' },
     { id: 'sd-estado-carga', label: 'SD05 Estado de Carga', type: 'subitem' },
     { id: 'sd-pedidos-especiales', label: 'SD06 Pedidos Especiales', type: 'subitem' },
+    { id: 'sd-comparativa-cd01', label: 'SD07 Comparativa CD01 vs WMS', type: 'subitem' },
   ]
   },
   {
